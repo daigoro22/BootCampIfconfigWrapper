7a1b1773f83c48a277b29e099cc32ae573d40ed4
daigoro <daigoro.abe@systemi.co.jp>
2022-05-16 15:54:03 +0900

    Add ifconfig wrapper module
    ifconfigのラッパーモジュールを追加

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,21 @@ module Lib
 
 import System.Process
 import System.IO
+import Data.Char
 
 someFunc :: IO ()
 someFunc = do
     (_, Just hOut, _, _) <- createProcess (proc "ifconfig" []) { std_out = CreatePipe }
     text <- hGetContents hOut
-    putStrLn text
+    putStrLn $ addHeader (lines text) ""
+
+addHeader :: [String] -> String -> String
+addHeader [] _ = ""
+addHeader (l:ls) h
+    | l =="" = '\n':l ++ residBody  ("") -- 空行の場合
+    | isLetter (head l) =  '\n':l ++ residBody(header) -- ヘッダがある行の場合
+    | otherwise = '\n':h ++ l ++ residBody(header) -- ヘッダ無しで空行以外の場合
+
+    where
+        header = if isLetter (head l)  then (fst $ span (/=':') l)++":" else h
+        residBody = addHeader(ls)
